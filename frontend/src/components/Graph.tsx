7448538385501fefe3c2bc8a1f32d11f2acea752
Richard.Oropesa <93955101+RichardOro01@users.noreply.github.com>
2023-05-08 00:58:01 -0400

    feat: options advance

--- frontend/src/components/Graph.tsx
+++ frontend/src/components/Graph.tsx
@@ -12,14 +12,14 @@ const MainGraph = () => {
   const [linking, setLinking] = useState<IdType | null>(null);
   const [graph, setGraph] = useState<graphData>({
       nodes: [
-        { id: 1, label: "Node 1", title: "node 1 tootip text" },
+        { id: 1, label: "Node 1", title: "node 1 tootip text", shadow: true, shape: 'circle' },
         { id: 2, label: "Node 2", title: "node 2 tootip text" },
         { id: 3, label: "Node 3", title: "node 3 tootip text" },
         { id: 4, label: "Node 4", title: "node 4 tootip text" },
         { id: 5, label: "Node 5", title: "node 5 tootip text" }
       ],
       edges: [
-        { from: 1, to: 2 },
+        { from: 1, to: 2, shadow: true },
         { from: 1, to: 3 },
         { from: 2, to: 4 },
         { from: 2, to: 5 },
@@ -79,7 +79,7 @@ const MainGraph = () => {
       if (linking) {
         if (event.nodes[0]) {
           setGraph((g)=>{
-            const newEdges = [...g.edges, {from: linking, to: event.nodes[0]}];
+            const newEdges = [...g.edges, {from: linking, to: event.nodes[0], shadow: true}];
             return {...g, edges: newEdges};
           })
         }
--- frontend/src/components/Options.tsx
+++ frontend/src/components/Options.tsx
@@ -1,13 +1,53 @@
-import { Button } from 'antd'
-import React from 'react'
+import styled from '@emotion/styled';
+import { Button, Form, FormInstance, Input, Modal } from 'antd'
+import React, { useRef, useState } from 'react'
+
+const ModalStyled = styled(Modal)`
+  .ant-btn-primary {
+    background-color: #1677ff ;
+  }
+`
 
 const Options = () => {
+  const [currentModal, setCurrentModal] = useState<string>();
+  const insertForm = useRef<FormInstance<any>>(null);
+  const handleInsert = () => {
+    insertForm.current?.validateFields()
+    .then(()=>{
+      setCurrentModal(undefined);
+    })
+    .catch(()=>{});
+  }
   return (
     <div className='border-r-2 flex flex-col gap-4 px-8 my-8 pt-4'>
         <Button>New graph</Button>
-        <Button>Insert node</Button>
+        <Button onClick={()=>setCurrentModal('insert_node')}>Insert node</Button>
         <Button>Import graph</Button>
         <Button>Export Graph</Button>
+        {currentModal==='insert_node' &&
+          <ModalStyled
+            title='Insert node'
+            centered
+            open={true}
+            onCancel={()=>setCurrentModal(undefined)}
+            onOk={handleInsert}
+          >
+            <Form
+              ref={insertForm}
+            >
+              <Form.Item
+                name='node_info'
+                label='Info'
+                rules={[
+                  {required: true, message: 'Select the info!'}
+                ]}
+              >
+                <Input/>
+              </Form.Item>
+            </Form>
+          </ModalStyled>
+        }
+        
     </div>
   )
 }
